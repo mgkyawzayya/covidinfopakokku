8136a707e23b4332197cd5a481c1dcc8d3b3d0fd
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2021-07-21 16:17:31 +0630

    Fix Blog Edit

--- resources/views/emergency.blade.php
+++ resources/views/emergency.blade.php
@@ -24,7 +24,7 @@
             @foreach ($emergencies as $emergency)
             <li class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
                 <div class="flex-1 flex flex-col p-8">
-                    <img class="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
+                    <img class="w-40 h-40 flex-shrink-0 mx-auto bg-black rounded-full"
                         src="{{ asset($emergency->path) }}" alt="">
                     <h3 class="mt-6 text-gray-900 text-sm font-medium">{{ $emergency->name }}</h3>
                     <p class="mt-1 text-gray-500 text-sm truncate">{{ $emergency->address }}</p>
