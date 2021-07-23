1feb2eb695ede145a59ac0461cfc47cf69c8b0df
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2021-07-23 14:30:18 +0630

    Added Download Link

--- resources/views/donation.blade.php
+++ resources/views/donation.blade.php
@@ -6,7 +6,7 @@
   The alpine.js code is *NOT* production ready and is included to preview
   possible interactivity
 -->
-<div class="bg-white py-8">
+<div class="bg-white py-0">
     <div class="bg-gray-100">
         <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
             <div class="text-center">
